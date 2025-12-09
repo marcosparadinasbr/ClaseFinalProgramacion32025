0464e15933c733fa9864657282c79a92bd94a91c
marcosparadinasbr <marcos.paradinas@usal.es>
2025-12-09 19:00:31 +0100

    feat(documentation): first documentation

---
+++ miprimerproyectomaven/src/main/java/com/usal/Main.java
@@ -0,0 +1,10 @@
+package com.usal;
+import com.google.gson.Gson;
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        Persona p= new Persona("Marcos", "Bermudo");
+        String personJSON = gson.toJson(p);
+        System.out.println(personJSON);
+    }
+}
---
+++ miprimerproyectomaven/src/main/java/com/usal/Persona.java
@@ -0,0 +1,41 @@
+package com.usal;
+
+/**
+ * Esta clase es muy importante.
+ * 
+ */
+public class Persona {
+    String nombre;
+    String apellido;
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * Esto es un método factory que sirve para crear una Persona a partir de un String con formato CSV
+     * @param personaString String separado por comas con el nombre y apellido
+     * @return retorna una Persona o lanza una @IllegalArgumentException si el formato es incorrecto
+     */
+    public static Persona fromCSVString(String personaString) {
+        String[] trozos = personaString.split(",");
+        if (trozos.length != 2) {
+            throw new IllegalArgumentException("Parámetros inválidos");
+        }
+        return new Persona(trozos[0], trozos[1]);
+    }
+}
